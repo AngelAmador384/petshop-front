af69e116b48aeb0b850f25ae74473d64dd07c6ca
AngelAmador384 <angelamador415@gmail.com>
2026-05-26 22:36:53 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+// 1. Cambia la importación a la versión nombrada (usando llaves):
+import { createPersistedState } from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
@@ -9,7 +10,8 @@ import './assets/main.css'
 const app = createApp(App)
 const pinia = createPinia()
 
-pinia.use(piniaPluginPersistedstate)
+// 2. Ejecuta la función creadora al usar el plugin:
+pinia.use(createPersistedState())
 
 app.use(pinia)
 app.use(router)
