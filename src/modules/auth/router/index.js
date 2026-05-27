08f3fa6a84bb2a89ccd8473482c9dfab52700594
AngelAmador384 <angelamador415@gmail.com>
2026-05-26 23:22:03 -0700

    Update index.js

--- src/modules/auth/router/index.js
+++ src/modules/auth/router/index.js
@@ -3,6 +3,7 @@ import SessionView from '@/modules/auth/views/SessionView.vue'
 const AuthRoutes = [
     { path: '/login', name: 'login', component: SessionView },
     { path: '/registro', name: 'registro', component: SessionView },
+    { path: '/confirmar/:token', name: 'ConfirmarCorreo', component: ConfirmarCorreo },
 ]
 
 export default AuthRoutes
